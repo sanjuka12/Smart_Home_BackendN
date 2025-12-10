ffb174f09d9753041b137cacbb5c372f93f6db1c
sanjuka12 <dishansanjuka3@gmail.com>
2025-12-10 21:42:54 +0530

    Updated

--- routes/inverterRoutes.js
+++ routes/inverterRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { saveInverterData, fetchInverterData, deleteAllInverterData} = require('../controllers/inverterController');
+const { saveInverterData, fetchInverterData, deleteAllInverterData, getTodayEnergy} = require('../controllers/inverterController');
 
 // POST /api/inverter
 router.post('/solarinverterdata', saveInverterData);
 router.get('/solarinverterdata', fetchInverterData);
 router.delete('/solarinverterdata', deleteAllInverterData);
+router.get("/today-energy", getTodayEnergy);
 
 
 
